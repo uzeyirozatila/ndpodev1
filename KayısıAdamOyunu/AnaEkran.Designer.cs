54a9a45a54377117eb0059ac0e7496c42d317950
uzeyir <uzeyir.ozatila44@gmail.com>
2021-04-07 21:32:37 +0300

    Form Tasarımı

---
+++ KayısıAdamOyunu/AnaEkran.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace KayısıAdamOyunu
+{
+    partial class AnaEkran
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AnaEkran));
+            this.BilgiPl = new System.Windows.Forms.Panel();
+            this.KahramanPl = new System.Windows.Forms.Panel();
+            this.SavasPl = new System.Windows.Forms.Panel();
+            this.BilgiLb = new System.Windows.Forms.Label();
+            this.SkorLb = new System.Windows.Forms.Label();
+            this.SkorBtn = new System.Windows.Forms.Button();
+            this.SkorShowLb = new System.Windows.Forms.Label();
+            this.BilgiPl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BilgiPl
+            // 
+            this.BilgiPl.BackColor = System.Drawing.SystemColors.Info;
+            this.BilgiPl.Controls.Add(this.SkorShowLb);
+            this.BilgiPl.Controls.Add(this.SkorBtn);
+            this.BilgiPl.Controls.Add(this.SkorLb);
+            this.BilgiPl.Controls.Add(this.BilgiLb);
+            this.BilgiPl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BilgiPl.Location = new System.Drawing.Point(0, 0);
+            this.BilgiPl.Name = "BilgiPl";
+            this.BilgiPl.Size = new System.Drawing.Size(1222, 88);
+            this.BilgiPl.TabIndex = 0;
+            // 
+            // KahramanPl
+            // 
+            this.KahramanPl.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.KahramanPl.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.KahramanPl.Location = new System.Drawing.Point(0, 745);
+            this.KahramanPl.Name = "KahramanPl";
+            this.KahramanPl.Size = new System.Drawing.Size(1222, 65);
+            this.KahramanPl.TabIndex = 1;
+            // 
+            // SavasPl
+            // 
+            this.SavasPl.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.SavasPl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SavasPl.Location = new System.Drawing.Point(0, 88);
+            this.SavasPl.Name = "SavasPl";
+            this.SavasPl.Size = new System.Drawing.Size(1222, 657);
+            this.SavasPl.TabIndex = 2;
+            // 
+            // BilgiLb
+            // 
+            this.BilgiLb.AutoSize = true;
+            this.BilgiLb.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BilgiLb.Location = new System.Drawing.Point(3, 9);
+            this.BilgiLb.Name = "BilgiLb";
+            this.BilgiLb.Size = new System.Drawing.Size(614, 72);
+            this.BilgiLb.TabIndex = 0;
+            this.BilgiLb.Text = "Oyunu başlatmak için ENTER tuşuna basınız.\r\nKahramanı haraket ettirmek için YÖN t" +
+    "uşlarını kullanınız.\r\nAteş etmek için BOŞLUK tuşuna basınız\r\n";
+            // 
+            // SkorLb
+            // 
+            this.SkorLb.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SkorLb.AutoSize = true;
+            this.SkorLb.Font = new System.Drawing.Font("Georgia", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.SkorLb.Location = new System.Drawing.Point(1099, 64);
+            this.SkorLb.Name = "SkorLb";
+            this.SkorLb.Size = new System.Drawing.Size(42, 17);
+            this.SkorLb.TabIndex = 2;
+            this.SkorLb.Text = "Skor";
+            // 
+            // SkorBtn
+            // 
+            this.SkorBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SkorBtn.Font = new System.Drawing.Font("Georgia", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.SkorBtn.Location = new System.Drawing.Point(863, 33);
+            this.SkorBtn.Name = "SkorBtn";
+            this.SkorBtn.Size = new System.Drawing.Size(167, 48);
+            this.SkorBtn.TabIndex = 3;
+            this.SkorBtn.Text = "En İyi Skorlar";
+            this.SkorBtn.UseVisualStyleBackColor = true;
+            // 
+            // SkorShowLb
+            // 
+            this.SkorShowLb.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SkorShowLb.AutoSize = true;
+            this.SkorShowLb.Font = new System.Drawing.Font("Georgia", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.SkorShowLb.Location = new System.Drawing.Point(1160, 63);
+            this.SkorShowLb.Name = "SkorShowLb";
+            this.SkorShowLb.Size = new System.Drawing.Size(52, 17);
+            this.SkorShowLb.TabIndex = 4;
+            this.SkorShowLb.Text = "label3";
+            // 
+            // AnaEkran
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1222, 810);
+            this.Controls.Add(this.SavasPl);
+            this.Controls.Add(this.KahramanPl);
+            this.Controls.Add(this.BilgiPl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AnaEkran";
+            this.Text = "Kayısı Adam Oyunu";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.BilgiPl.ResumeLayout(false);
+            this.BilgiPl.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel BilgiPl;
+        private System.Windows.Forms.Button SkorBtn;
+        private System.Windows.Forms.Label SkorLb;
+        private System.Windows.Forms.Label BilgiLb;
+        private System.Windows.Forms.Panel KahramanPl;
+        private System.Windows.Forms.Panel SavasPl;
+        private System.Windows.Forms.Label SkorShowLb;
+    }
+}
+
--- KayısıAdamOyunu/AnaEkran.cs
+++ KayısıAdamOyunu/AnaEkran.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace KayısıAdamOyunu
 {
-    public partial class Form1 : Form
+    public partial class AnaEkran : Form
     {
-        public Form1()
+        public AnaEkran()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
         }
     }
 }
